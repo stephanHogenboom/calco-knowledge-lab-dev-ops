079ddadc57da6e97c9718aa5e81ecd5eb4cf19e0
Stephan Hogenboom <stephan.hogenboom@dhl.com>
2018-01-29 15:36:40 +0100

    last minute fix

--- src/main/java/com/stephanHogenboom/App.java
+++ src/main/java/com/stephanHogenboom/App.java
@@ -83,7 +83,7 @@ public class App extends Application {
         primaryStage.setY(bounds.getMinY());
         primaryStage.setWidth(bounds.getWidth());
         primaryStage.setHeight(bounds.getHeight());
-        primaryStage.initStyle(StageStyle.UNIFIED);
+        primaryStage.initStyle(StageStyle.UTILITY);
         window = primaryStage;
         window.setTitle("Calco DevOps");
         window.setMinHeight(1000);
